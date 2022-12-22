2b28a812fa936aa1c40e745280b665fb1012e9d4
jffpzm <118867882+jffpzm@users.noreply.github.com>
2022-12-21 23:39:55 -0500

    Update lpu.sh

--- lpu.sh
+++ lpu.sh
@@ -2,7 +2,7 @@
 # Web Host Log Parsing Utility
 # @filename lpu.sh
 # @created 2022.08.23
-# @version 2022.12.21+15:15
+# @version 2022.12.22+00:00
 
 # Configure global logging
 log_file="/var/log/lpu.log" # Path to log file
@@ -11,12 +11,18 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>$log_file 2>&1
 # Everything below will be logged to "$log_file":
 
+
+### Function Definitions Part 1 ###
+await() { echo -e "\n\n$1"; read -rsn1 -p "Press any key to continue" && echo -e "\n\n"; }
+get_timestamp() { date +"%Y-%m-%d+%H:%M:%S"; }
+###
+
+### Configuration Information ###
+start_timestamp=$(date +"%Y%m%d_%H%M%S")
+echo -e "\n[INFO] Execution of ${script_name} started at ${start_timestamp}"
 relative_script_path="$0"
 absolute_script_path="$(readlink -f $0)"
 script_name="${absolute_script_path##*/}"
-echo -e "\n[INFO] Execution of ${script_name} started at $(date +"%Y-%m-%d_%H:%M:%S")"
-
-### Configuration Information ###
 base_directory="$(dirname "${absolute_script_path}")" #"/opt/lpu" # Base working directory 
 config_file="${base_directory}/lpu.conf" #"/etc/lpu.conf" # Path to config file 
 temp_directory="${base_directory}/tmp" # Temporary working directory
@@ -25,13 +31,16 @@ package_directory="${base_directory}/deliverable" # Directory to save reports in
 term_period="month" # Length of term to parse, e.g. hour, day, month, year, or all 
 term_target="last month" # String datetime describing a point within the desired term_period
 # Hashing preferences
-# Must be set before any hashing is done, clearly
 host_hash_start=1
 host_hash_stop=12
 site_hash_start=1
 site_hash_stop=24
 
-debug_initial_configuration=$(cat <<EOF
+
+# Source config if it exists
+[ -f $config_file ] && source $config_file
+
+debug_configuration=$(cat <<EOF
 Default Configuration:
 	relative_script_path = $relative_script_path
 	absolute_script_path = $absolute_script_path
@@ -48,15 +57,47 @@ Default Configuration:
 	site_hash_stop = $site_hash_stop
 EOF
 )
-echo "[INFO] ${debug_initial_configuration}" 
+echo "[INFO] ${debug_configuration}" 
 
-# Source config if it exists
-[ -f $config_file ] && source $config_file
-
-### Function definitions ###
-await() { echo -e "\n\n$1"; read -rsn1 -p "Press any key to continue" && echo -e "\n\n"; }
-site_hash_function() { echo "$1" | sha256sum | cut -c "${site_hash_start}-${site_hash_stop}"; }
+### Function Definitions Part 2 ###
+hash_site() { echo "$1" | sha256sum | cut -c "${site_hash_start}-${site_hash_stop}"; }
 host_hash_function() { echo "$1" | sha256sum | cut -c "${host_hash_start}-${host_hash_stop}"; }
+mask_all_matches() { 
+	local unmasked_string
+	local masked_string 
+	local file_to_mask
+	for arg in "$@"; do
+        case "$arg" in
+            -u=*|--unmasked-string=*) unmasked_string="${arg#*=}" ;;
+			-m=*|--masked-string=*) masked_string="${arg#*=}" ;;
+			-f=*|--file-to-mask=*) file_to_mask="${arg#*=}" ;;
+            *) ;;
+        esac
+    done
+	#[ -n $unmasked_string ] && echo "Unmasked string: $unmasked_string"
+	#[ -n $masked_string ] && echo "Masked string: $masked_string"
+	#[ -n $file_to_mask ] && echo "File to mask: $file_to_mask"
+	#[ -z $unmasked_string ] && echo "Unmasked string: <UNSET>"
+	#[ -z $masked_string ] && echo "Masked string: <UNSET>"
+	#[ -z $file_to_mask ] && echo "File to mask: <UNSET>"
+	[ ! -f $file_to_mask ] && echo "File does not exist"
+	[ -f $file_to_mask ] && sed -i "s/${unmasked_string}/${masked_string}/gi" "$file_to_mask"
+}
+append_all_lines() {
+	local suffix_string
+	local file_to_append
+	for arg in "$@"; do
+        case "$arg" in
+            -s=*|--suffix-string=*) suffix_string="${arg#*=}" ;;
+			-f=*|--file-to-append=*) file_to_append="${arg#*=}" ;;
+            *) ;;
+        esac
+    done
+	[ ! -f $file_to_append ] && echo "File does not exist"
+	[ -f $file_to_append ] && sed -i "s/$/ ${suffix_string}/" "$file_to_append"
+}
+strip_all_extensions () { echo $1 | sed -e 's/\.[^.][^.]*$//'; }
+strip_gz_extension () { echo $zipped_log | sed -e 's/\.gz$//'; }
 ##
 
 ### Initialization ###
@@ -92,7 +133,6 @@ iso_date=$(date -d "$term_target" +%F) # term_target's full date; like %+4Y-%m-%
 short_date="${iso_date//-/}"
 timezone=$(date -d "$term_target" +%Z) # Alphabetic time zone abbreviation, e.g. EST
 # Generate a runtime timestamp
-timestamp=$(date +"%Y%m%d_%H%M%S") # Timestamp
 
 debug_time_info=$(cat <<EOF
 Datetime Information:
@@ -121,6 +161,11 @@ echo "[INFO] ${debug_host_hash}"
 
 
 ##### Main Process #####
+## TESTING: Pull host logs before the loop so we can obfuscate them at the same time
+
+[ -f /var/log/apache2/access_log ] && cp "/var/log/apache2/access_log" "${temp_directory}/host/apache2_access_log"
+
+
 ## Pull access logs from each site's home directory into $base_directory
 case "$term_period" in 
 	d|day|Day) cp /home/*/logs/*-${month}-${year}.gz ./ ;;
@@ -137,7 +182,7 @@ for zipped_log in *.gz; do
     gunzip "$zipped_log"
 
     # Strip the '.gz' extension from zipped_log and save it for reference
-    unzipped_log=$(echo $zipped_log | sed -e s/\.gz$//) 
+    unzipped_log=$(strip_gz_extension "$zipped_log") #$(echo $zipped_log | sed -e s/\.gz$//) 
 
     # Extract logs from the previous day into a separate file in $temp_directory
 	case "$term_period" in 
@@ -148,13 +193,13 @@ for zipped_log in *.gz; do
 	esac
 
 	# Strip the TLD from the FQDN to get a bare domain name 
-	current_site=$(echo $unzipped_log | sed -e 's/\.[^.][^.]*$//')
+	current_site=$(strip_all_extensions "$unzipped_log") #$(echo $unzipped_log | sed -e 's/\.[^.][^.]*$//')
 	
 	# Append $current_site to the $site_names array
 	site_names+=($current_site)
 	
 	# Hash the current sitename
-	hashed_site=$(site_hash_function "$current_site")
+	hashed_site=$(hash_site "$current_site")
 	
 ### Only used for debugging ###
 debug_site_info=$(cat <<	EOF
@@ -169,11 +214,14 @@ EOF
 ###############################
 	
 	# Append site and hostname to the end of each line in the logfile 
-	sed -i "s/$/ ${hashed_site}/" "${temp_directory}/sites/${unzipped_log}"
+	append_all_lines --suffix-string="$hashed_site" --file-to-append="${temp_directory/sites/${unzipped_log}"
+	#sed -i "s/$/ ${hashed_site}/" "${temp_directory}/sites/${unzipped_log}"
 	#sed -i "s/$/ ${hashed_site} ${hashed_host}/" "${temp_directory}/sites/${unzipped_log}"  # <-- Maybe used later
 	
 	# Mask instances of the hostname occurring in the logs <-- Does not mask any instances outside of own logs
-	sed -i "s/${current_site}/${hashed_site}/gi" "${temp_directory}/sites/${unzipped_log}"
+	mask_all_matches --unmasked-string="$current_site" --masked-string="$hashed_site" --file-to-mask="${temp_directory}/sites/${unzipped_log}"
+	mask_all_matches --unmasked-string="$current_site" --masked-string="$hashed_site" --file-to-mask="${temp_directory}/host/apache2_access_log"
+	#sed -i "s/${current_site}/${hashed_site}/gi" "${temp_directory}/sites/${unzipped_log}"
 	
 	# Rename logfile with hashed domain name
 	mv $temp_directory/sites/$unzipped_log $temp_directory/sites/$hashed_site
@@ -195,15 +243,16 @@ echo "[INFO] Exiting for-loop at $(date +"%Y-%m-%d_%H:%M:%S")"
 echo "[INFO] Emptying trash"
 rm -rf "${temp_directory}/trash"
 
-### Create deliverable archive 
-echo "[INFO] Creating archive and compressing"
-cp $log_file $temp_directory 
-( cd $temp_directory && tar -czf "${package_directory}/${hashed_host}_${timestamp}_${term_period}_of_${short_date}.tar.gz" * )
 
 # Clean up working directory before we finish up
 echo "[INFO] Tidying up working directory"
 rm -rf $temp_directory
 rm *.gz
+
+### Create deliverable archive 
+echo "[INFO] Creating archive and compressing"
+cp $log_file $temp_directory 
+( cd $temp_directory && tar -czf "${package_directory}/${hashed_host}_${timestamp}_${term_period}_of_${short_date}.tar.gz" * )
 
 # Exit gracefully
 echo "[INFO] Execution of ${script_name} complete at $(date +"%Y-%m-%d_%H:%M:%S")"
