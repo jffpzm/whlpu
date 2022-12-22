75d590b0799ee9ed80b6423c769e3e1d5a3c7036
jffpzm <118867882+jffpzm@users.noreply.github.com>
2022-12-22 11:25:44 -0500

    Update lpu.sh

--- lpu.sh
+++ lpu.sh
@@ -139,7 +139,6 @@ Datetime Information:
 	month = $month 
 	dom = $dom 
 	iso_date = $iso_date 
-	timezone = $timezone 
 	timestamp = $start_timestamp
 EOF
 )
@@ -176,9 +175,10 @@ for zipped_log in *.gz; do
     gunzip "$zipped_log"
 
     # Strip the '.gz' extension from zipped_log and save it for reference
-    unzipped_log=$(echo $zipped_log | sed -e s/\.gz$//) 
-
-    # Extract logs from the previous day into a separate file in $temp_directory
+    #unzipped_log=$(echo $zipped_log | sed -e s/\.gz$//) 
+	unzipped_log="$(strip_gz_extension "$zipped_log")"
+    
+	# Extract logs from the previous day into a separate file in $temp_directory
 	case "$term_period" in 
 		d|day|Day) grep "${dom}/${month}/${year}" $unzipped_log >> "${temp_directory}/sites/${unzipped_log}" ;;
 		m|month|Month) grep ".*/${month}/${year}" $unzipped_log >> "${temp_directory}/sites/${unzipped_log}" ;;
@@ -187,7 +187,8 @@ for zipped_log in *.gz; do
 	esac
 
 	# Strip the TLD from the FQDN to get a bare domain name 
-	current_site=$(echo $unzipped_log | sed -e 's/\.[^.][^.]*$//')
+	#current_site=$(echo $unzipped_log | sed -e 's/\.[^.][^.]*$//')
+	current_site="$(strip_all_extensions "$unzipped_log")"
 	
 	# Append $current_site to the $site_names array
 	site_names+=($current_site)
@@ -208,11 +209,13 @@ EOF
 ###############################
 	
 	# Append site and hostname to the end of each line in the logfile 
-	sed -i "s/$/ ${hashed_site}/" "${temp_directory}/sites/${unzipped_log}"
+	#sed -i "s/$/ ${hashed_site}/" "${temp_directory}/sites/${unzipped_log}"
+	append_all_lines --suffix-string="$hashed_site" --file-to-append="${temp_directory/sites/${unzipped_log}"
 	#sed -i "s/$/ ${hashed_site} ${hashed_host}/" "${temp_directory}/sites/${unzipped_log}"  # <-- Maybe used later
 	
 	# Mask instances of the hostname occurring in the logs <-- Does not mask any instances outside of own logs
-	sed -i "s/${current_site}/${hashed_site}/gi" "${temp_directory}/sites/${unzipped_log}"
+	#sed -i "s/${current_site}/${hashed_site}/gi" "${temp_directory}/sites/${unzipped_log}"
+	mask_all_matches --unmasked-string="$current_site" --masked-string="$hashed_site" --file-to-mask="${temp_directory}/sites/${unzipped_log}"
 	
 	# Rename logfile with hashed domain name
 	mv $temp_directory/sites/$unzipped_log $temp_directory/sites/$hashed_site
